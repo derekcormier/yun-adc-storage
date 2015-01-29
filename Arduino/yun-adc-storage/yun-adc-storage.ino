34f6e4dac2b8a16f16dfefe148252d1f8738c186
Derek Cormier <derek@derekcormier.com>
2015-01-29 00:31:20 -0500

    Added /collect and /stop REST endpoints to start and stop collection of data.

--- Arduino/yun-adc-storage/yun-adc-storage.ino
+++ Arduino/yun-adc-storage/yun-adc-storage.ino
@@ -1,19 +1,59 @@
+#include<Bridge.h>
 #include<Process.h>
+#include<YunServer.h>
+#include<YunClient.h>
 #include<math.h>
+
+YunServer server;
+bool getData = false;
 
 void setup() {
   Bridge.begin();  
   Serial.begin(9600);
+  
+  server.listenOnLocalhost();
+  server.begin();
 }
 
 void loop() {
-  String request = "http://192.168.1.10:3000/rec/";
-  while(request.length() < 300) {
-    delay(150);
-    request += convertToBase32(analogRead(0));
+  YunClient client = server.accept();
+  
+  if(client) {
+    processClientRequest(client);
+   
+    client.stop(); 
   }
-  sendRequest(request);
+  
+  if(getData == true) {
+    Serial.println("Recording...");
+    
+    String request = "http://192.168.1.10:3000/rec/";
+  
+    while(request.length() < 300) {
+      delay(150);
+      request += convertToBase32(analogRead(0));
+    }
+  
+    sendRequest(request);
+  }
 }
+
+
+void processClientRequest(YunClient client) {
+  String request = client.readString();
+  request.trim();
+  
+  if(request == "collect") {
+    getData = true;
+    client.print(F("Starting data collection"));
+  }
+  
+  if(request == "stop") {
+    getData = false;
+    client.print(F("Stopping data collection"));
+  }
+}
+
 
 String writeValue(String request,int ADCValue) {
   String value = convertToBase32(ADCValue);
@@ -21,15 +61,15 @@ String writeValue(String request,int ADCValue) {
   return request + value;
 }
 
+
 void sendRequest(String request) {
   Process p;
-  
-  Serial.println(request);
   
   p.begin("curl");
   p.addParameter(request);
   p.run();
 }
+
   
 String convertToBase32(int decVal) {
   String base32Val = "";
